7365c36051617d5ed1cf53273695158a87ebaa32
chiarafais <chiara0915@gmail.com>
2024-05-29 17:00:09 +0200

    es 3 ok

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,21 @@ public class Main {
 
     List<Product> prodotti = generaProdotti();
         System.out.println(prodotti);
-
+        
+        //es 1
         List<Product> filterBook = prodotti.stream().filter(product -> product.getCategory().equals(Category.BOOKS) && product.getPrice() > 100.00).toList();
-        System.out.println("lista di Books con un costo maggiore a €100" + filterBook);
+        System.out.println("lista di Books con un costo maggiore a €100 : " + filterBook);
 
+        //es 2 da sistemare, non avevo letto bene la consegna
+        List<Product> filterBaby = prodotti.stream().filter(product -> product.getCategory().equals(Category.BABY)).toList();
+        System.out.println("lista di ordini categoria Baby : " + filterBaby);
 
+        //es 3
+        List<Product> productsBoys = prodotti.stream().filter(product -> product.getCategory().equals(Category.BOYS)).map(product -> {
+            product.setPrice(product.getPrice() * 0.9);
+            return product;
+        }).toList();
+        System.out.println("prodotti boys scontati al 10% : "+ productsBoys);
     }
     public static List<Product> generaProdotti(){
         List<Product> prodotti = new ArrayList<Product>();
