937c54b7a8370af76e19740336f6ff35e3fa45d3
chiarafais <chiara0915@gmail.com>
2024-05-29 15:10:18 +0200

    first part

---
+++ src/esercises/Product.java
@@ -0,0 +1,16 @@
+package esercises;
+
+public class Product {
+
+    private long id;
+    private String name;
+    private String category;
+    private double price;
+
+    public Product(long id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+}
